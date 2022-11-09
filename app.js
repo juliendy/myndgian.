ba1221a8ad77a46820ea6cd386417b3bd787e91f
juliendy <julien.yasar@gmail.com>
2022-11-09 21:01:25 +0100

    feat: html: new items, style: styling on lists, js: dynamic header

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 // Toggle Light/Dark
 const sunIcon = document.getElementById("sunIcon");
 const moonIcon = document.getElementById("moonIcon");
-const toggleInner = document.querySelector(".toggle-inner");
-const root = document.querySelector(":root");
+const toggleInner = document.querySelector(".toggle-inner")
+const root = document.querySelector(":root")
 
-sunIcon.addEventListener("click", toggleDarkMode);
-moonIcon.addEventListener("click", toggleLightMode);
+sunIcon.addEventListener("click", toggleDarkMode)
+moonIcon.addEventListener("click", toggleLightMode)
 
 function toggleDarkMode() {
-    toggleInner.style.transform = "translateY(-50%)";
-    root.style.setProperty("--color-white", "#333333");
-    root.style.setProperty("--color-black", "#ffffff");
-    root.style.setProperty("--color-dark", "#f1f1f1");
-    root.style.setProperty("--color-light", "#444444");
+    toggleInner.style.transform = "translateY(-50%)"
+    root.style.setProperty("--color-white", "#333333")
+    root.style.setProperty("--color-black", "#ffffff")
+    root.style.setProperty("--color-dark", "#f1f1f1")
+    root.style.setProperty("--color-light", "#444444")
 }
 
 function toggleLightMode() {
     toggleInner.style.transform = "translateY(0)";
-    root.style.setProperty("--color-white", "#ffffff");
-    root.style.setProperty("--color-black", "#333333");
-    root.style.setProperty("--color-dark", "#444444");
-    root.style.setProperty("--color-light", "#f1f1f1");
+    root.style.setProperty("--color-white", "#ffffff")
+    root.style.setProperty("--color-black", "#333333")
+    root.style.setProperty("--color-dark", "#444444")
+    root.style.setProperty("--color-light", "#f1f1f1")
 }
+
+// To-Do List (Added dynamic header)
+let listName = 'My To-Do List'
+let userName = 'User'
+let listedTodos = 0
+let completedTodos = 0
+
+const listNameText = document.getElementById('listNameText')
+
+listNameText.textContent = listName
