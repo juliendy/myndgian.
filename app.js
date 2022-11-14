1752d1588f9ddcdee189ef5d84186cc8c39ae75e
juliendy <julien.yasar@gmail.com>
2022-11-14 13:38:01 +0100

    feat: functionality to modal with xp; styling mini components.

--- app.js
+++ app.js
@@ -51,6 +51,8 @@ function toggleDarkMode() {
     root.style.setProperty("--color-black", "#ffffff");
     root.style.setProperty("--color-dark", "#f1f1f1");
     root.style.setProperty("--color-light", "#444444");
+    root.style.setProperty("--color-complete", "rgb(69, 112, 87)");
+    root.style.setProperty("--color-priority", "rgb(87, 87, 1)");
 }
 
 function toggleLightMode() {
@@ -59,6 +61,8 @@ function toggleLightMode() {
     root.style.setProperty("--color-black", "#333333");
     root.style.setProperty("--color-dark", "#444444");
     root.style.setProperty("--color-light", "#f1f1f1");
+    root.style.setProperty("--color-complete", "rgb(69, 112, 87)");
+    root.style.setProperty("--color-priority", "rgb(255, 255, 205)");
 }
 
 // Toggle 'Settings' Modal
@@ -146,25 +150,25 @@ showCompleteBtn.forEach((showComplete) => {
 
 // 4. Toggle Incomplete:
 showIncompleteBtn.forEach((showIncomplete) => {
-    showIncomplete.addEventListener('click', () => {
-        showIncomplete.classList.add('filter-active');
+    showIncomplete.addEventListener("click", () => {
+        showIncomplete.classList.add("filter-active");
 
         showAllBtn.forEach((showAll) => {
-            showAll.classList.remove('filter-active');
+            showAll.classList.remove("filter-active");
         });
         showCompleteBtn.forEach((showComplete) => {
-            showComplete.classList.remove('filter-active');
+            showComplete.classList.remove("filter-active");
         });
         showPrioritisedBtn.forEach((showPrioritised) => {
-            showPrioritised.classList.remove('filter-active');
+            showPrioritised.classList.remove("filter-active");
         });
 
-        const listItem = document.querySelectorAll('.to-do-item');
+        const listItem = document.querySelectorAll(".to-do-item");
         listItem.forEach((item) => {
-            if (item.classList.contains('item-complete')) {
-                item.style.display = 'none';
+            if (item.classList.contains("item-complete")) {
+                item.style.display = "none";
             } else {
-                item.style.display = 'flex';
+                item.style.display = "flex";
             }
         });
     });
@@ -172,25 +176,25 @@ showIncompleteBtn.forEach((showIncomplete) => {
 
 // 5. Toggle 'Prioritised':
 showPrioritisedBtn.forEach((showPrioritised) => {
-    showPrioritised.addEventListener('click', () => {
-        showPrioritised.classList.add('filter-active');
+    showPrioritised.addEventListener("click", () => {
+        showPrioritised.classList.add("filter-active");
 
         showAllBtn.forEach((showAll) => {
-            showAll.classList.remove('filter-active');
+            showAll.classList.remove("filter-active");
         });
         showCompleteBtn.forEach((showComplete) => {
-            showComplete.classList.remove('filter-active');
+            showComplete.classList.remove("filter-active");
         });
         showIncompleteBtn.forEach((showIncomplete) => {
-            showIncomplete.classList.remove('filter-active');
+            showIncomplete.classList.remove("filter-active");
         });
 
-        const listItem = document.querySelectorAll('.to-do-item');
+        const listItem = document.querySelectorAll(".to-do-item");
         listItem.forEach((item) => {
-            if (item.classList.contains('priority-item')) {
-                item.style.display = 'flex';
+            if (item.classList.contains("priority-item")) {
+                item.style.display = "flex";
             } else {
-                item.style.display = 'none';
+                item.style.display = "none";
             }
         });
     });
@@ -255,6 +259,7 @@ function checkListedToDos() {
 }
 
 // Add a To-Do
+const smallAddBtn = document.getElementById("smallAddBtn");
 const addItemBtn = document.querySelectorAll("#addItemBtn");
 const addItemInput = document.querySelectorAll("#addItemInput");
 let inputFieldActive = false;
@@ -262,6 +267,8 @@ let inputFieldActive = false;
 addItemBtn.forEach((addBtn) => {
     addBtn.addEventListener("click", addItemToList);
 });
+
+smallAddBtn.addEventListener("click", openAddModal);
 
 //--Add Item to List on 'Enter'--
 document.addEventListener("keydown", function () {
@@ -382,6 +389,20 @@ function addNewItemToList() {
     starIcon.forEach((star) => {
         star.addEventListener("click", checkClickedIcon);
     });
+
+    // ---------- 🛠 Edit Button 🛠 --------------
+    const editIcon = document.querySelectorAll(".fa-edit");
+
+    editIcon.forEach((editBtn) => {
+        editBtn.addEventListener("click", checkClickedIcon);
+    });
+
+    // ---------- ❌ Delete Button ❌ --------------
+    const deleteIcon = document.querySelectorAll(".fa-trash-alt");
+
+    deleteIcon.forEach((deleteBtn) => {
+        deleteBtn.addEventListener("click", checkClickedIcon);
+    });
 }
 
 function checkClickedIcon(e) {
@@ -419,7 +440,10 @@ function checkClickedIcon(e) {
     }
 
     // If blank star, change to filled (prioritised) star:
-    else if (icon.classList.contains("far", "fa-star")) {
+    else if (
+        icon.classList.contains("far") &&
+        icon.classList.contains("fa-star")
+    ) {
         if (iconItem.classList.contains("item-complete")) {
             // Nothing happens
         } else {
@@ -432,7 +456,10 @@ function checkClickedIcon(e) {
     }
 
     // If filled (prioritised) star, change to blank star:
-    else if (icon.classList.contains("fas", "fa-star")) {
+    else if (
+        icon.classList.contains("far") &&
+        icon.classList.contains("fa-star")
+    ) {
         if (iconItem.classList.contains("item-complete")) {
             // Nothing happens
         } else {
@@ -440,6 +467,129 @@ function checkClickedIcon(e) {
             icon.classList.add("far");
             iconItem.classList.remove("priority-item");
             prioritisedTodos--;
+            updateSummary();
+        }
+    }
+
+    // If delete icon, open modal to delete
+    else if (icon.classList.contains("fa-trash-alt")) {
+        openDeleteModal(iconItem);
+    }
+    // If edit icon, open modal to edit
+    else if (icon.classList.contains("fa-edit")) {
+        if (iconItem.classList.contains("item-complete")) {
+            // Nothing happens
+        } else {
+            openEditModal();
+        }
+    }
+
+    // Open Default Modal
+    const modal = document.querySelector(".modal");
+    const modalInner = document.querySelector(".modal-inner");
+
+    function openModal() {
+        window.scrollTo(0, 0);
+        body.classList.toggle("body-fixed");
+        modal.style.display = "flex";
+        modalInner.style.display = "flex";
+    }
+
+    function closeModal() {
+        body.classList.remove("body-fixed");
+        modal.style.display = "none";
+        modalInner.style.display = "none";
+        currentModal = "";
+    }
+
+    modalInner.addEventListener("click", (e) => {
+        const clickedArea = e.target;
+
+        if (clickedArea.classList.contains("modal-inner")) {
+            closeModal();
+        }
+    });
+
+    // Customise Modal (Delete)
+    const modalHeader = document.querySelector(".modal-header");
+    const headerSpan = document.querySelector(".header-span");
+    const modalText1 = document.querySelector(".modal-text-1");
+    const modalText2 = document.querySelector(".modal-text-2");
+    const modalInput = document.querySelector(".modal-input");
+    const modalBtn = document.querySelector(".btn-main");
+    const btnCancel = document.querySelector(".btn-cancel");
+    const btnClose = document.querySelector(".btn-close");
+    let currentModal;
+
+    btnCancel.addEventListener("click", closeModal);
+    btnClose.addEventListener("click", closeModal);
+    window.addEventListener("keydown", (e) => {
+        const pressedKey = e.key;
+
+        if (pressedKey === "Escape") {
+            closeModal();
+        }
+    });
+
+    function openAddModal() {
+        openModal();
+        currentModal = "add";
+
+        modalHeader.innerHTML =
+            '<span class="header-span" style="color: var(--color-primary)">Add</span> A To-Do Item';
+        modalText1.textContent = "Write a new to-do entry below:";
+        modalText2.style.display = "none";
+        modalInput.style.display = "flex";
+        modalInput.placeholder = "Add a to-do...";
+        modalBtn.style.backgroundColor = "var(--color-primary)";
+        modalBtn.textContent = "Add";
+    }
+    function openEditModal() {
+        openModal();
+        currentModal = "edit";
+
+        modalHeader.innerHTML =
+            '<span class="header-span" style="color: var(--color-primary)">Edit</span> Your To-Do Item';
+        modalText1.textContent = "You can update your to-do item below:";
+        modalText2.style.display = "none";
+        modalInput.style.display = "flex";
+        modalInput.placeholder = "Whatever the item says";
+        modalBtn.style.backgroundColor = "var(--color-primary)";
+        modalBtn.textContent = "Edit";
+    }
+
+    function openDeleteModal(item) {
+        const thisItem = item;
+
+        openModal();
+        currentModal = "delete";
+
+        modalHeader.innerHTML =
+            '<span class="header-span" style="color: var(--color-negative)">Delete</span> Your To-Do Item?';
+        modalText1.textContent =
+            "You will be unable to recover the data once it is deleted.";
+        modalText2.textContent = "Are you sure?";
+        modalInput.style.display = "none";
+        modalBtn.style.backgroundColor = "var(--color-negative)";
+        modalBtn.textContent = "Delete";
+
+        window.addEventListener("keydown", (e) => {
+            const pressedKey = e.key;
+
+            if (pressedKey === "Enter" && currentModal === "delete") {
+                console.log("Deleting");
+                closeModal();
+                deleteItem(thisItem);
+            }
+        });
+    }
+
+    function deleteItem(itemToDelete) {
+        if (itemToDelete.classList.contains("item-complete")) {
+            itemToDelete.remove();
+        } else {
+            listedTodos--;
+            itemToDelete.remove();
             updateSummary();
         }
     }
